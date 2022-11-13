e66a6d1210d8c0c9bb95ff18eca533f8ed51e294
Alexandro Abade <alexandroabade@gmail.com>
2022-11-13 18:02:22 -0300

    added [system 'clear'] to clean screen in the method purchase

--- projeto_01/e-commerce.rb
+++ projeto_01/e-commerce.rb
@@ -1,18 +1,41 @@
-loop do
-
 puts "********************* E-COMMERCE INOVA.ACAO AFRO - CESAR ********************"
 puts "*"
 puts "*"
-puts "Selecione a opção desejada:\n[ 1 ] Comprar\n[ 2 ] Sair"
-print 'Opção: '
 
-opcao_desjada = gets.chomp.to_i
-puts "\n"
+loop do
+  puts "Selecione a opção desejada:\n[ 1 ] Comprar\n[ 2 ] Sair"
+  print 'Opção: '
+  opcao_desejada = gets.to_i
+  puts "\n"
+# ____________________ end menu e-commerce ____________________ 
+
+def purchase
+  puts "Selecione a opção desejada: "
+  puts "[ 1 ] Sticker - Penguin Linux: R$39,00"
+  puts "[ 2 ] Sticker - Ruby on Rails: R$4,99"
+  puts "[ 3 ] Sticker - Java Script: R$3,99"
+  print 'Opção: '
+  product = gets.to_i
+
+  if product == 1 || product == 2 || product == 3  
+    print "Digite a quantidade desejada: "
+    amount = gets.to_i
+    subtotal = amount ** 2 
+    puts "Subtotal: R$#{subtotal.to_f}"
+    print "Digite [ 0 ] para voltar ao menu inicial: "
+    back = gets.to_i
+    if back == 0
+      system "clear"
+    end
+  end
+end 
+# ____________________ end method purchase ____________________
+
 
 def goodbye
   count = 0
   print 'Finalizando'
-  while count < 5
+  while count < 3
     sleep 1
     print "."
     count += 1
@@ -25,9 +48,12 @@ def goodbye
 end
 
   case
-  when opcao_desjada == 2 
+  when opcao_desejada == 1
+    purchase
+  when opcao_desejada == 2 
     goodbye
     break
   end
+  # ____________________ end method goodbye and case option purchase ____________________
 
 end
